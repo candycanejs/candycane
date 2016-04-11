2b498f19b5dd34e323ba7684711972b405fa536f
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-11 00:00:33 -0500

    Simple IoC container

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,8 @@
 const Babel = require('broccoli-babel-transpiler');
 
 const babelScript = Babel('lib', {
+  "presets": ["es2015-node5", "stage-0"],
+  "plugins": ["transform-decorators-legacy"]
 });
 
 module.exports = babelScript;
